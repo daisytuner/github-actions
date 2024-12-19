4cb443508eaf25a8028ae7ec154e123ac0f5ba7c
Lukas Truemper <lukas.truemper@outlook.de>
2024-12-19 14:47:24 +0100

    Restructured inputs

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 import * as axios from "axios";
+import * as yaml from "yaml";
 
 async function run() {
   try {
     // Read inputs
     const baseImage = core.getInput("base_image");
-    const script = core.getInput("script");
     const timeout = parseInt(core.getInput("timeout")) * 1000 * 60; // convert minutes to ms
-    const benchmarks = JSON.parse(core.getInput("benchmarks"));
+    const build = core.getInput("build");
+    const stage = core.getInput("stage");
+    const run = yaml.parse(core.getInput("run"));
+    if (!baseImage || !build || !stage || !run) {
+      throw new Error("Missing required inputs.");
+    }
+
+    for (const [name, command] of Object.entries(run)) {
+      core.info(`Collected run command for benchmark ${name}: ${command}`);
+    }
 
     const repoUrl = github.context.payload.repository?.git_url;
     const repoName = github.context.payload.repository?.name;
@@ -19,9 +28,9 @@ async function run() {
     }
 
     const commitSha = github.context.sha;
-    const runNumber = github.context.runNumber;
     const actor = github.context.actor;
-    if (!commitSha || !actor) {
+    const runNumber = github.context.runNumber;
+    if (!commitSha || !actor || !runNumber) {
       throw new Error("Missing PR information in the github context");
     }
 
@@ -32,16 +41,18 @@ async function run() {
       repo_owner: repoOwner,
       is_private: isPrivate,
       commit_sha: commitSha,
-      run_number: runNumber,
       actor: actor,
+      run_number: runNumber,
       base_image: baseImage,
-      script: script,
-      benchmarks: benchmarks
+      build: build,
+      stage: stage,
+      run: run,
+      timeout: timeout,
     };
 
     // Post to Firebase function
     core.info("Submitting job to the backend...");
-    const response = await axios.default.post("https://create-bhqsvyw3sa-uc.a.run.app", payload, {
+    const response = await axios.default.post("https://createjob-bhqsvyw3sa-uc.a.run.app", payload, {
       headers: { "Content-Type": "application/json" },
     });
 
@@ -52,56 +63,6 @@ async function run() {
   
     const jobId = response.data.jobId;
     core.info(`Job posted successfully! Job ID: ${jobId}`);
-
-    // Poll job status
-    core.info(`Waiting for a runner to pickup the job...`);
-    const endTime = Date.now() + timeout;
-    let status = "pending";
-    let lastLogLength = 0;
-    while (status !== "completed" && status !== "failed") {
-      if (Date.now() > endTime) {
-        throw new Error(`Action timed out after ${timeout / 1000 } seconds.`);
-      }
-
-      const statusResponse = await axios.default.post(
-        "https://status-bhqsvyw3sa-uc.a.run.app",
-        { jobId },
-        {
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-      if (statusResponse.status !== 200) {
-        throw new Error(`Failed to fetch job status. Status: ${statusResponse.status}`);
-      }
-
-      const job = statusResponse.data;
-      if (status === "pending" && job.status !== "pending") {
-        core.info(`Runner has picked up the job!`);
-        status = job.status;
-      }
-
-      // Print new log output
-      if (job.log) {
-        const newLog = job.log.slice(lastLogLength); // Get only the new part of the log
-        if (newLog) {
-          core.info(`${newLog}`);
-        }
-        lastLogLength = job.log.length;
-      }
-
-      if (job.status === "completed" || job.status === "failed") {
-        core.info(`Job ${jobId} has completed with status: ${job.status}`);
-        if (job.status === "failed") {
-          core.setFailed(`Job ${jobId} failed.`);
-        }
-        status = job.status;
-      } else {
-        // Wait before polling again
-        await new Promise((resolve) => setTimeout(resolve, 1000)); // Poll every 1 second
-      }
-    }
   } catch (error: any) {
     core.error(`An error occurred: ${error.message}`);
     core.setFailed(error.message);
